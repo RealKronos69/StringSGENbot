e307bff328a4fb46d57dbb36894fc15129d4cbb3
RealKronos69 <138235386+RealKronos69@users.noreply.github.com>
2023-10-12 22:59:36 +0530

    Update config.py

--- config.py
+++ config.py
@@ -14,5 +14,5 @@ MONGO_DB_URI = getenv("MONGO_DB_URI", None)
 OWNER_ID = int(getenv("OWNER_ID", 5494845582))
 SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/Spartans_Mainchat")
 START_IMG_URL = getenv(
-    "START_IMG_URL", "https://telegra.ph/file/a6d4c0c71a705cd7e92a2.jpg"
+    "START_IMG_URL", https://telegra.ph/file/67d154254214ba4ad51c9.jpg"
 )
